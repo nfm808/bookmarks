077a54415ff7bc7b208d2e97533cb21dba23ec20
Nathan Fowler <luxurysandbox@gmail.com>
2019-06-18 13:51:59 -1000

    set up testing scaffold

---
+++ bookmarks_server/test/bookmarks-endpoints.spec.js
@@ -0,0 +1,16 @@
+const { expect } = require('chai')
+const knex = require('knex')
+const app = require('../src/app')
+const { makeBookmarksArray } = require('./bookmarks.fixtures')
+
+describe('Bookmarks Endpoints', () => {
+  let db
+
+  before('make knex instance', () => {
+    db = knex({
+      client: 'pg',
+      connection: process.env.TEST_DB_URL,
+    })
+    app.search('db', db)
+  })
+})
