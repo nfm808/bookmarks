72b2d1f21bedd78e6ae0960726e299f8eb8a2fd3
Nathan Fowler <luxurysandbox@gmail.com>
2019-06-30 08:32:59 -1000

    updated for production

--- bookmarks_client/bookmarks-app/src/config.js
+++ bookmarks_client/bookmarks-app/src/config.js
@@ -1,4 +1,4 @@
 export default {
-  API_ENDPOINT: process.env.REACT_APP_API_URL,
+  API_ENDPOINT: "https://bookmarks-full-stack-server.herokuapp.com/api/bookmarks",
   API_KEY: process.env.REACT_APP_API_KEY,
 }
